3b83e3e55f797da204da91bb964d728fa0791f4a
Max Kaplan <kaplanmaxe3@gmail.com>
2018-04-14 17:21:09 -0400

    ADDING all server methods

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,37 @@ import { ElectrumUtil } from '../util';
 import { IElectrumRequestBody } from '../';
 
 class Server {
+
+  static async version(request: IElectrumRequestBody) {
+    const result = await ElectrumUtil.makeRequest({
+      socket: request.socket,
+      id: request.id,
+      method: 'server.version',
+      params: request.params || [],
+    });
+    return result;
+  }
+
+  static async banner(request: IElectrumRequestBody) {
+    const result = await ElectrumUtil.makeRequest({
+      socket: request.socket,
+      id: request.id,
+      method: 'server.banner',
+      params: request.params || [],
+    });
+    return result;
+  }
+
+  static async donationAddress(request: IElectrumRequestBody) {
+    const result = await ElectrumUtil.makeRequest({
+      socket: request.socket,
+      id: request.id,
+      method: 'server.donation_address',
+      params: request.params || [],
+    });
+    return result;
+  }
+
   static async peersSubscribe(request: IElectrumRequestBody) {
     const result = await ElectrumUtil.makeRequest({
       socket: request.socket,
--- test/server/server.spec.ts
+++ test/server/server.spec.ts
@@ -4,10 +4,33 @@ import { Server } from '../../src';
 import { TestUtils } from '../util';
 
 describe('Electrum server method tests', () => {
+  it('should receive version', async () => {
+    const socket = await TestUtils.constructSocket('185.64.116.15', 50002);
+    const response = await Server.version({ socket, id: 1 });
+    TestUtils.closeSocket(socket);
+    expect(response.result).to.contain('Electrum');
+  });
+
+  it('should receive banner', async () => {
+    const socket = await TestUtils.constructSocket('185.64.116.15', 50002);
+    const response = await Server.banner({ socket, id: 1 });
+    TestUtils.closeSocket(socket);
+    expect(response.result).to.equal('Welcome to Electrum!');
+  });
+
+  it('should receive donation address', async () => {
+    const socket = await TestUtils.constructSocket('185.64.116.15', 50002);
+    const response = await Server.donationAddress({ socket, id: 1 });
+    TestUtils.closeSocket(socket);
+    const prefixes = ['b', '1', '3'];
+    expect(prefixes.indexOf(response.result.charAt(0))).to.be.greaterThan(-1);
+  });
+
   it('should receive a list of peers', async () => {
     const socket = await TestUtils.constructSocket('185.64.116.15', 50002);
     const response = await Server.peersSubscribe({ socket, id: 1 });
     TestUtils.closeSocket(socket);
-    expect(response).to.have.property('result');
+    expect(typeof response.result).to.equal('object');
+    expect(response.result[0][0]).to.contain('.');
   });
 });
